9c2a89555e1dd90a4296ee6e65d1e9970cb85e3b
Milan Pjevic <milanpjevic@icloud.com>
2024-11-17 09:32:54 +0100

    Buildig the Static: Form - Split the Bill

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import Button from "./components/Button";
 import FormAddFriend from "./components/FormAddFriend";
+import FormSplitBill from "./components/FormSplitBill";
 import FriendsList from "./components/FriendsList";
 
 export default function App() {
@@ -12,6 +13,8 @@ export default function App() {
         <FormAddFriend />
         <Button>Add friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
