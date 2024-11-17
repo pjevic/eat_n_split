eb57dea57472e514ab4127b99378d4410951d8d1
Milan Pjevic <milanpjevic@icloud.com>
2024-11-17 10:16:57 +0100

    Displaying the New Friend Form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 /** @format */
 
+import { useState } from "react";
+
 import Button from "./components/Button";
 import FormAddFriend from "./components/FormAddFriend";
 import FormSplitBill from "./components/FormSplitBill";
 import FriendsList from "./components/FriendsList";
 
 export default function App() {
+  const [showAddFriend, setShowFriend] = useState(false);
+
+  function handleToggle() {
+    setShowFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleToggle}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
 
       <FormSplitBill />
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
 /** @format */
 
-export default function Button({ children }) {
-  return <button className="button">{children}</button>;
+export default function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
 }
